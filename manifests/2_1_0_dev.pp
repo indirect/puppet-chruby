c91d5a4398048896ff0d32b11c4fd305c8d9988f
Andre Arko <andre@arko.net>
2013-11-06 17:37:41 -0500

    add 2.1.0-dev

--- manifests/2_1_0_dev.pp
+++ manifests/2_1_0_dev.pp
@@ -1,15 +1,15 @@
-# Installs ruby 2.1.0-preview1.
+# Installs ruby 2.1.0-dev.
 
-class ruby::2_1_0_preview1 {
+class ruby::2_1_0_dev {
   require openssl
 
-  ruby::version { '2.1.0-preview1': }
+  ruby::version { '2.1.0-dev': }
 
   if $::operatingsystem == 'Darwin' {
     include homebrew::config
     include autoconf
 
-    Ruby::Version['2.1.0-preview1'] {
+    Ruby::Version['2.1.0-dev'] {
       require => Package['autoconf'],
       env => {
         'RUBY_CONFIGURE_OPTS' => "--with-openssl-dir=${homebrew::config::installdir}/opt/openssl",
