55f54ba2bfa38015a30eec76406bc128c5c29139
Andre Arko <andre@arko.net>
2013-11-09 11:30:03 -0500

    require autoconf correctly

--- manifests/2_1_0_dev.pp
+++ manifests/2_1_0_dev.pp
@@ -2,15 +2,14 @@
 
 class ruby::2_1_0_dev {
   require openssl
+  require autoconf
 
   ruby::version { '2.1.0-dev': }
 
   if $::operatingsystem == 'Darwin' {
     include homebrew::config
-    include autoconf
 
     Ruby::Version['2.1.0-dev'] {
-      require => Package['autoconf'],
       env => {
         'RUBY_CONFIGURE_OPTS' => "--with-openssl-dir=${homebrew::config::installdir}/opt/openssl",
       }
--- manifests/2_1_0_preview1.pp
+++ manifests/2_1_0_preview1.pp
@@ -2,15 +2,14 @@
 
 class ruby::2_1_0_preview1 {
   require openssl
+  require autoconf
 
   ruby::version { '2.1.0-preview1': }
 
   if $::operatingsystem == 'Darwin' {
     include homebrew::config
-    include autoconf
 
     Ruby::Version['2.1.0-preview1'] {
-      require => Package['autoconf'],
       env => {
         'RUBY_CONFIGURE_OPTS' => "--with-openssl-dir=${homebrew::config::installdir}/opt/openssl",
       }
