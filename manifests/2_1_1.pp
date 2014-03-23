e6a914b0ed68598b5e6421d1fa1a8c38764be025
Andre Arko <andre@arko.net>
2014-03-23 10:21:52 -0700

    ruby 2.1 and 2.1.1

--- manifests/2_1_1.pp
+++ manifests/2_1_1.pp
@@ -1,15 +1,15 @@
-# Installs ruby 2.1.0
+# Installs ruby 2.1.1
 
-class ruby::2_1_0 {
+class ruby::2_1_1 {
   require openssl
   require autoconf
 
-  ruby::version { '2.1.0': }
+  ruby::version { '2.1.1': }
 
   if $::operatingsystem == 'Darwin' {
     include homebrew::config
 
-    Ruby::Version['2.1.0'] {
+    Ruby::Version['2.1.1'] {
       env => {
         'RUBY_CONFIGURE_OPTS' => "--with-openssl-dir=${homebrew::config::installdir}/opt/openssl",
       }
