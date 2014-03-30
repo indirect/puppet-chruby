3c3914770b00e27d87e16ac64f7a36a56df68542
Andre Arko <andre@arko.net>
2014-03-30 18:44:19 +0800

    link 2.0.0-p451 as 2.0.0

--- manifests/2_0_0.pp
+++ manifests/2_0_0.pp
@@ -1,16 +1,16 @@
-# Installs ruby 2.0.0-p353 via ruby-build and symlinks it as 2.0.0.
+# Installs ruby 2.0.0-p451 via ruby-build and symlinks it as 2.0.0.
 #
 # Usage:
 #
 #     include ruby::2_0_0
 class ruby::2_0_0 {
   require ruby
-  require ruby::2_0_0_p353
+  require ruby::2_0_0_p451
 
   file { "${ruby::chruby_root}/versions/2.0.0":
     ensure  => symlink,
     force   => true,
-    target  => "${ruby::chruby_root}/versions/2.0.0-p353"
+    target  => "${ruby::chruby_root}/versions/2.0.0-p451"
   }
 }
 
